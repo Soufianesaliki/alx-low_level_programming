517fed0a75f7794db72d4d95ff6358eef60416a6
Soufianesaliki <soufianesaliki1@gmail.com>
2023-02-21 07:46:23 -0800

    0

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,11 +14,11 @@ int main(void)
 	j = 1;
 	k = 2;
 
-	printf ("%lu", j);
+	printf("%lu", j);
 
 	for (i = 1; i < 91; i++)
 	{
-		printf (", %lu", k);
+		printf(", %lu", k);
 		k = k + j;
 		j = k - j;
 	}
@@ -30,15 +30,15 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf (", %lu", k1 + (k2 / 1000000000));
-		printf ("%lu", k2 % 1000000000);
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
 		k2 = k2 + j2;
 		j2 = k2 - j2;
 	}
 
-	printf ("\n");
+	printf("\n");
 
 	return (0);
 }
