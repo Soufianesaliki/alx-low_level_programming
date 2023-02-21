9f0b6978ea6cf54c7b48667a4b88976c68f20f5b
Soufianesaliki <soufianesaliki1@gmail.com>
2023-02-21 05:53:33 -0800

    one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * main - Entry point
+ * Description: prints '-putchar' followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char word[]="_putchar\n";
+	int i=0;
+
+	while (word[i] != '\0')
+	{
+		_putchar(word[i]);
+		i++;
+	} 
+	return (0);
+}
