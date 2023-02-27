654ecf0c8729c7ba78a57d0b16ee2df880556dc2
Soufianesaliki <soufianesaliki1@gmail.com>
2023-02-26 23:28:15 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 takes a pointer then turns it is value to 98
+ * @n integer pointer to a variable
+ *
+ * return : nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n=98;
+}
