52e001aed8365b46855eefe689de3b51eea49c50
Soufianesaliki <soufianesaliki1@gmail.com>
2023-03-06 08:10:56 -0800

    00à

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        int i = 0;
+	unsigned int i;
 
-	while (i < n)
-        {
-	        s[i] = b;
-		i++;
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
 	}
-        return (s);
+	return (s);
 }
