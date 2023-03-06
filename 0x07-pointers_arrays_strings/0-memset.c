8f0f5d4bb267b3bc33a3a2949a1a8e022b6411a2
Soufianesaliki <soufianesaliki1@gmail.com>
2023-03-06 07:23:49 -0800

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s:string
+ * @b:char
+ * @n:unsigned integer
+ * Return:pointer to string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *d = s;
+	int i = 0;
+
+	while ((*s != '\0') && (i < n))
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+	return d;
+}
+
