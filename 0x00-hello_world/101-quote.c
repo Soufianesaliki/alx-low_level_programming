5c738001a2f78dd9e05705e9f7caa74f0efb7c28
soufianesaliki <soufianesaliki1@gmail.com>
2023-07-13 12:22:01 -0700

    finaly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 
 /**
  * main - Printing a quote to the std error
- * quote: a string containing the quote
+ * q: a string containing the quote
  * Return: 1
 */
 
 int main(void)
 {
-	char quote[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char q[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, &quote, strlen(quote));
+	write(2, &q, strlen(q));
 	return (1);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,14 @@
 
 /**
  * main - printing a msg
+ * m: a string containing the message
  * Return: 0
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	char m[80] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", m);
 	return (0);
 }
