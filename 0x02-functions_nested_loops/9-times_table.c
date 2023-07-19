ec791a816ae0804b6c6977bc55a3a1737130994e
soufianesaliki <soufianesaliki1@gmail.com>
2023-07-19 14:14:19 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,31 +7,27 @@
 
 void times_table(void)
 {
-	int i, j, p;
-	char n, m;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (j = 1; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			p = i * j;
-			if ((p / 10) > 0)
+			if (((i * j) / 10) > 0)
 			{
-				_putchar('0' + (p / 10));
+				_putchar('0' + ((i * j) / 10));
 			}
 			else
 			{
 				_putchar(' ');
 			}
-			_putchar('0' + (p % 10));
-			if (j < 9)
+			_putchar('0' + ((i * j) % 10));
+			if (j == 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				continue;
 			}
+			_putchar(',');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
