97135625b251e03d0e9e34caee966527c4a211f9
soufianesaliki <soufianesaliki1@gmail.com>
2023-07-20 04:34:08 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	if (size >= 0)
 	{
 		for (i = 0; i < size; i++)
 		{
@@ -21,5 +21,4 @@ void print_square(int size)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
