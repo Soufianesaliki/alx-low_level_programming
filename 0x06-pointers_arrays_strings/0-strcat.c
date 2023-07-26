1ac1e96ae4b2dcc624aeae59d6a545aa17c6780b
soufianesaliki <soufianesaliki1@gmail.com>
2023-07-26 10:23:32 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,6 @@ char *_strcat(char *dest, char *src)
 {
 	int i, l = 0;
 
-	if (dest == NULL && src == NULL)
-		return (NULL);
-	if (dest == NULL)
-		return (src);
-	if (src == NULL)
-		return (dest);
 	while (dest[l] != '\0')
 		l++;
 	while (src[i] != '\0')
