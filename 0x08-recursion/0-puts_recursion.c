9eb44b56a1c82c6d1d72adbfeb245f6aa20120bc
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-02 03:17:21 -0700

    0-puts_recursion.c fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * puts_recursion - prints a string, followed by a new line
+ * _puts_recursion - prints a string,
+ * followed by a new line
  * @s: string
  * Return: nothing
  */
 
 void _puts_recursion(char *s)
 {
+	if (s == NULL)
+		return;
 	_putchar(*s);
 	s++;
 	if (*s != '\0')
 		_puts_recursion(s);
 	else
 		_putchar('\n');
-	return;
 }
