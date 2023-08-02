b303e78d24ca310896c1c6457fe13f5f6604358a
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-02 03:13:01 -0700

    pushing the source file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_recursion - prints a string, followed by a new line
+ * @s: string
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+		_puts_recursion(s);
+	else
+		_putchar('\n');
+	return;
+}
