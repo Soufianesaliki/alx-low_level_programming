41d31b7b30108230d0aeb59838fded5e47d056c3
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-07 04:37:42 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	if (s1 == NULL)
+	if (s1 == NULL && s2 != NULL)
 		return (s2);
-	if (s2 == NULL)
+	if (s2 == NULL && s1 != NULL)
 		return (s1);
 	for (i = 0; s1[i]; i++)
 		sz_str1++;
