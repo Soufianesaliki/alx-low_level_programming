27583873adbb6cfa9beabba0cbf749dd71341e8a
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-07 04:44:07 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,12 +14,15 @@ char *str_concat(char *s1, char *s2)
 	int sz_str1 = 0, sz_str2 = 1;
 	char *new_str;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	if (s1 == NULL && s2 != NULL)
-		return (s2);
-	if (s2 == NULL && s1 != NULL)
-		return (s1);
+	if (s1 == NULL || s2 == NULL)
+	{
+		if (s2 != NULL)
+			return (s2);
+		else if (s1 != NULL)
+			return (s1);
+		else
+			return (NULL);
+	}
 	for (i = 0; s1[i]; i++)
 		sz_str1++;
 	for (i = 0; s2[i]; i++)
