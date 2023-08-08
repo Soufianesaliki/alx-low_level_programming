4659205e3822fdf579029b9175c585bb3d9ce6c2
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-08 09:26:43 -0700

    2, 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,40 @@
 #include "main.h"
 
 /**
- * str_concat - concatenates two strings
- * @s1: first srtring
+ * str_concat - concatenates two strings.
+ * @s1: first string
  * @s2: second string
- * Return:	newly allocated space in memory has s1 & s2
- *		NULL if failed or s1 & s2 empty
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-	int sz, sz_str1 = 0, sz_str2 = 0;
+	int i, j, sz1, sz2, sz;
 	char *new_str;
 
+	sz1 = sz2 = 0;
+
 	if (s1 != NULL)
-		for (i = 0; s1[i]; i++)
-			sz_str1++;
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			sz1++;
+	}
 	if (s2 != NULL)
-		for (i = 0; s2[i]; i++)
-			sz_str2++;
-	sz = sz_str1 + sz_str2;
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+			sz2++;
+	}
+	sz = sz1 + sz2;
 	new_str = (char *)malloc(sizeof(char) * (sz + 1));
 	if (new_str == NULL)
 		return (NULL);
-	i = 0;
-	while (s1[i] != '\0')
-	{
+	for (i = 0; i < sz1; i++)
 		new_str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
+	for (j = 0; j < sz2; j++, i++)
 		new_str[i] = s2[j];
-		i++;
-		j++;
-	}
 	new_str[i] = '\0';
 	return (new_str);
 }
+
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,7 +34,7 @@ int **alloc_grid(int width, int height)
 		}
 	}
 	for (i = 0; i < height; i++)
-	{		
+	{
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
