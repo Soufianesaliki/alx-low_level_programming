f92e74309cab0b40cc10c2f6eb228518ece4938b
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-10 05:45:04 -0700

    1-string_nconcat.c no debuging

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,33 +18,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 != NULL)
 		for (i = 0; s1[i]; i++)
 			sz1++;
-	printf("sz1 = %d\n", sz1);
 	if (s2 != NULL)
 		for (i = 0; s2[i]; i++)
 			sz2++;
-	printf("sz2 = %d\n", sz2);
 	if (n < sz2)
 		sz2 = n;
-	printf("sz2 after n = %d\tn = %d\n", sz2, n);
 	sz = sz1 + sz2;
-	printf("sz = %d\n", sz);
 	concat = (char *)malloc(sizeof(char) * (sz + 1));
 	if (concat == NULL)
-	{
-		printf("failed\n");
 		return (NULL);
-	}
 	for (i = 0; i < sz1; i++)
-	{
 		concat[i] = s1[i];
-		printf("concat[%d] = %c\ts1[%d] = %c\n", i, concat[i], i, s1[i]);
-	}
 	for (j = 0; j < sz2; j++, i++)
-	{
 		concat[i] = s2[j];
-		printf("concat[%d] = %c\ts2[%d] = %c\n", i, concat[i], j, s2[j]);
-	}
 	concat[i] = '\0';
-	printf("concat[%d] = %c\n", i, concat[i]);
 	return (concat);
 }
