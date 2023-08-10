7babff86512aadcc9cf3663f3b1124ddcdedef20
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-10 09:56:24 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _realloc - creates and frees a memory block
+ * @ptr: void pointer
+ * @old_size: unsigned int
+ * @new_size: unsigned int
+ * Return:	Void pointer
+ *		NULL if ptr != NULL and new_size = 0
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i, limit;
+	char *new_ptr;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (new_ptr);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size < old_size)
+		limit = new_size;
+	else
+		limit = old_size;
+	for (i = 0; i < limit; i++)
+		new_ptr[i] = *((char *)ptr + i);
+	return (new_ptr);
+}
