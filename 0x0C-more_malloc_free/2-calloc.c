a59424ebf67d07f391d66b54931866925adbdde6
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-10 07:51:32 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *array;
 
 	if (nmemb == 0 || size == 0)
-		return (0);
+		return (NULL);
 	array = (char *)malloc(nmemb * size);
 	if (array == NULL)
 		return (NULL);
