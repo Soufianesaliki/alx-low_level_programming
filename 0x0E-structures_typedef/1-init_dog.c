be79e6c21ad66d59992b08ec68d9bdee01c34f14
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-14 05:43:25 -0700

    1-init_dog.c & dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,22 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int i, len;
-
-	for (i = 0; name[i]; i++)
-		len++;
-	d->name = (char *)malloc(sizeof(char) * len);
-	if (d->name == NULL)
-		return;
-	len = 0;
-	for (i = 0; owner[i]; i++)
-		len++;
-	d->owner = (char *)malloc(sizeof(char) * len);
-	if (d->owner == NULL)
-		return;
-	for (i = 0; name[i]; i++)
-		d->name[i] = name[i];
-	d->age = age;
-	for (i = 0; owner[i]; i++)
-		d->owner[i] = owner[i];
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
