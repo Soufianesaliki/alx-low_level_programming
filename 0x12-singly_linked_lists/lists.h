a23e48b14fb7662790dedd5985f8b76e3b049253
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-24 04:15:51 -0700

    add _putchar prototype to list.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct list_s
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
