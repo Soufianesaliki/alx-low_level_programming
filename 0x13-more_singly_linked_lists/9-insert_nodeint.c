2f71f30646187f3bba4192072d7a692a65f90cce
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-29 08:12:46 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	while (idx - 1 > i)
 	{
+		if (tmp == NULL)
+			return (NULL);
 		tmp = tmp->next;
 		i++;
 	}
