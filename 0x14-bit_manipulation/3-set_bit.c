d31e55140891e7dbe10624cc4b75f29d40450349
soufianesaliki <soufianesaliki1@gmail.com>
2023-08-31 11:32:14 -0700

    3, 4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,6 +11,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i = 1 << index;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
 	*n = *n | i;
 	return (1);
 }
